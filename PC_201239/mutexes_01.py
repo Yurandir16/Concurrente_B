76e4fadad63cd11c9f08676e9b0a695551abf118
Yurandir16 <yurandir2019@gmail.com>
2022-09-21 23:08:39 -0500

    teard commit

--- PC_201239/mutexes_01.py
+++ PC_201239/mutexes_01.py
@@ -1,29 +1,32 @@
-from threading import Thread, Semaphore
+import threading
 from pytube import YouTube
-semaforo = Semaphore(1)
 
+
+mutex = threading.Lock()
 
 def descarga(vid):
     yt = YouTube(vid)
     video=  yt.streams.filter(file_extension='mp4').order_by('resolution').first()
     video.download(destino)
-    print("el video se ha descargado en: "+destino)
+    print("el video descargado en: "+destino)
 
-class Hilo(Thread):
+class Hilo(threading.Thread):
     def __init__(self, id):
-        Thread.__init__(self)
+        threading.Thread.__init__(self)
         video=id
         self.id=video
 
-    def run(self):
-        semaforo.acquire()
-        descarga(self.id)
-        semaforo.release()
 
-destino=r"C:/Users/Yurandier/OneDrive/Escritorio/python/videos"
-threads_semaphore = [Hilo('https://www.youtube.com/watch?v=hoQmSA6MRAk'), Hilo('https://www.youtube.com/watch?v=jgXIT9ewqhk'), Hilo('https://www.youtube.com/watch?v=GO5utuvcZps'), Hilo('https://www.youtube.com/watch?v=NLyD2K8yLS0'),
+    def run(self):
+        mutex.acquire()
+        descarga(self.id)
+        mutex.release()
+
+destino="C:/Users/Yurandier/OneDrive/Escritorio/python/videos"
+
+Hilos = [Hilo('https://www.youtube.com/watch?v=hoQmSA6MRAk'), Hilo('https://www.youtube.com/watch?v=jgXIT9ewqhk'), Hilo('https://www.youtube.com/watch?v=GO5utuvcZps'), Hilo('https://www.youtube.com/watch?v=NLyD2K8yLS0'),
 Hilo('https://www.youtube.com/watch?v=nwrqQ2jYpwY'), Hilo('https://www.youtube.com/watch?v=6GCNUeTFSbA'), Hilo('https://www.youtube.com/watch?v=HzdD8kbDzZA'), Hilo('https://www.youtube.com/watch?v=xkmgcvr5Ezc'), Hilo('https://www.youtube.com/watch?v=mK7CI3XHYjs'),
 Hilo('https://www.youtube.com/watch?v=2sMGX_52_hs')]
 
-for t in threads_semaphore:
-    t.start()
+for h in Hilos:
+    h.start()
